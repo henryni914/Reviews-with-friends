24eab0afd1ba72cf733e5f134a78f1f1543cd35f
Henry Ni <henryni914@gmail.com>
2021-01-11 17:46:08 -0500

    testing semantic ui navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './pages/Home';
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+        {/* <h1>Hello</h1> */}
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   );
 }
 
