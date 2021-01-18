86d0582552dc95bd26470e2666958dd7a2b4fb56
Henry Ni <henryni914@gmail.com>
2021-01-17 23:24:43 -0500

    able to conduct the API call on moviepage and render the page with movie details

--- client/src/components/MovieItem.js
+++ client/src/components/MovieItem.js
@@ -7,23 +7,18 @@ import { setFilm } from '../actions/movies'
 export default function MovieItem(props) {
     // console.log('props', props.id)
     let posterUrl = ("https://image.tmdb.org/t/p/original" + props.poster)
-    let filmId = props.id
     const dispatch = useDispatch();
-    const storeMovies = useSelector(state => state.movies)
+    const storeMoviesArr = useSelector(state => state.movies.searchResults)
     function storeId(id) {
-        console.log('id',id)
-        // let selected = stateMovies.filter(element => element.id === id);
         dispatch(setFilm(id));
+        // Finds the current selected film inside the array and returns the movie id === selected id
+        // let selected = storeMoviesArr.filter(element => element.id === id);
         // console.log('selected', selected);
     }
-
-    // useEffect(() => {
-    //     storeId()
-    // }, []);
     
     return (
         <Item >
-            <Link onClick={() => storeId(filmId)} to={`/film/id=${props.id}`} >
+            <Link onClick={() => storeId(props.id)} to={`/film/id=${props.id}`} >
                 <img
                     class='search-img'
                     src={posterUrl}
@@ -31,7 +26,7 @@ export default function MovieItem(props) {
                 ></img>
             </Link>
             <Item.Content>
-                <Link to={`/film/id=${props.id}`} >
+                <Link onClick={() => storeId(props.id)} to={`/film/id=${props.id}`} >
                     <Item.Header as='a' >{props.title}</Item.Header>
                 </Link>
                 <Item.Description>
