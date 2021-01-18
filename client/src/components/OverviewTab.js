21cd8f8e124d18068215ccfc4fd9ae29b2867e58
Henry Ni <henryni914@gmail.com>
2021-01-18 14:46:44 -0500

    added a container to conditionally render depending on which menu tab is selected

---
+++ client/src/components/OverviewTab.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function OverviewTab() {
+
+    return(
+        <h1>Overview</h1>
+    )
+}
--- client/src/pages/MoviePage.js
+++ client/src/pages/MoviePage.js
@@ -1,16 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import API from '../utils/API';
-import { Breadcrumb, Container, Image, Segment } from 'semantic-ui-react';
+import { Breadcrumb, Container, Divider, Grid, Header, Image, Input, Menu, Segment } from 'semantic-ui-react';
+import Overview from '../components/OverviewTab';
+import Cast from '../components/CastTab';
 
 export default function MoviePage() {
 
     const stateMovie = useSelector(state => state.movies);
     const filmId = stateMovie.currentFilmId;
     const [results, setResults] = useState([])
+    const [tab, setTab] = useState("overview")
     const backdrop = "https://image.tmdb.org/t/p/original" + results.backdrop_path
-    // concat logo_path to below url to get provider logo
-    // https://image.tmdb.org/t/p/original
+    const handleTabChange = page => {
+        setTab(page)
+    };
+
+    const renderComponent = component => {
+        switch(tab){
+            case "overview": {
+                return <Overview />
+            }
+            case "cast": {
+                return <Cast />
+            }
+        }
+    }
 
     useEffect(() => {
         API.findByMovieId(filmId).then(res => {
@@ -26,29 +41,44 @@ export default function MoviePage() {
         <>
             {/* style={{height: '500px'}} */}
             {/* <Image fluid src={backdrop} ></Image> */}
+            <Container >
+                <Grid.Row>
+                    <Grid stackable>
+                        <Grid.Column width={6}>
+                            <Image src={backdrop} size='large' alt={results.original_title}></Image>
+                        </Grid.Column>
+
+                        <Grid.Column width={10}>
+                            <Header>{results.original_title}</Header>
+                            <>
+                                <p><i>{results.tagline}</i></p>
+                                <p>Testing</p>
+                                <p>Testing</p>
+                                <p>Testing</p>
+                            </>
+                        </Grid.Column>
+                    </Grid>
+                </Grid.Row>
+                <Divider hidden></Divider>
+            </Container>
+            
             <Container textAlign='center'>
-                <Image src={backdrop} size='large'></Image>
-                <Breadcrumb>
-                    <Breadcrumb.Section link active>Home</Breadcrumb.Section>
-                    <Breadcrumb.Divider>/</Breadcrumb.Divider>
-                    <Breadcrumb.Section link>Registration</Breadcrumb.Section>
-                    <Breadcrumb.Divider>/</Breadcrumb.Divider>
-                    <Breadcrumb.Section link>Personal Information</Breadcrumb.Section>
-                </Breadcrumb>
-                <Segment.Group>
-                    <Segment>Top</Segment>
-                    <Segment.Group>
-                        <Segment>Nested Top</Segment>
-                        <Segment>Nested Middle</Segment>
-                        <Segment>Nested Bottom</Segment>
-                    </Segment.Group>
-                    <Segment.Group horizontal>
-                        <Segment>Top</Segment>
-                        <Segment>Middle</Segment>
-                        <Segment>Bottom</Segment>
-                    </Segment.Group>
-                    <Segment>Bottom</Segment>
-                </Segment.Group>
+                <Menu attached='top' tabular>
+                    <Menu.Item
+                        name='overview'
+                        active={tab === 'overview'}
+                        onClick={() => handleTabChange("overview")}
+                    />
+                    <Menu.Item
+                        name='cast'
+                        active={tab === 'cast'}
+                        onClick={() => handleTabChange("cast")}
+                    />
+                </Menu>
+                {/* Below will be conditionally rendered depending on which tab is 'active' */}
+                <Segment attached='bottom'>
+                    {renderComponent(tab)}
+                </Segment>
             </Container>
         </>
     )
