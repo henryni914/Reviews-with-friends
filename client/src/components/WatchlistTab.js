ecc0fd2668b5518a950137a7f8662b7f0e2f95d4
Henry Ni <henryni914@gmail.com>
2021-04-16 17:20:34 -0400

    able to use buttons to set favorites to true or false and render properly

--- client/src/components/WatchlistTab.js
+++ client/src/components/WatchlistTab.js
@@ -1,26 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Card, Icon, Image, Tab } from 'semantic-ui-react';
+import { setUserWatchlist } from '../actions/user'
 
 export default function TabPane(props) {
     // console.log(props.movies)
-
+    const dispatch = useDispatch();
     const stateUser = useSelector(state => state.user);
-    const [movies, setMovies] = useState(stateUser.watchlist)
+    const [movies, setMovies] = useState(stateUser.watchlist);
 
     const notCompleted = movies.filter(el => {
         if (el.completed === false) {
             return el
         }
     })
-    console.log(notCompleted)
+    // console.log(notCompleted)
 
     const completed = movies.filter(el => {
         if (el.completed === true) {
             return el
         }
     })
-    console.log(completed)
+    // console.log(completed)
+
+    function setCompletedTrue(id) {
+        const movieArr = movies
+        const updateArr = movieArr.map(el => {
+            if (el.id === id) {
+                el.completed = true
+            }
+            return el
+        })
+        dispatch(setUserWatchlist(updateArr))
+        setMovies(updateArr)
+    }
+
+    function setCompletedFalse(id) {
+        console.log('test')
+        console.log(id)
+        const movieArr = movies
+        const updateArr = movieArr.map(el => {
+            if (el.id === id) {
+                el.completed = false
+            }
+            return el
+        })
+        dispatch(setUserWatchlist(updateArr))
+        setMovies(updateArr)
+    }
+    // console.log(movies)
+
 
 
     return (
@@ -37,19 +66,8 @@ export default function TabPane(props) {
                                         size='medium'
                                         src={el.Movie.image}
                                     />
-                                    {props.tab === 'watchlist' && (
-                                        <>
-                                            <Button content='Add to finished' labelPosition='left' icon='edit' primary id={el.id} />
-                                            <Button content='Remove' labelPosition='left' icon='edit' primary />
-                                        </>
-                                    )}
-
-                                    {props.tab === 'completed' && (
-                                        <>
-                                            <Button content='Move back to incomplete' labelPosition='left' icon='edit' primary />
-                                            <Button content='Remove' labelPosition='left' icon='edit' primary />
-                                        </>
-                                    )}
+                                    <Button content='Add to finished' labelPosition='left' icon='edit' primary onClick={() => setCompletedTrue(el.id)} />
+                                    <Button content='Remove' labelPosition='left' icon='edit' primary />
                                 </Card>
                             ))
                             :
@@ -59,25 +77,30 @@ export default function TabPane(props) {
                                         size='medium'
                                         src={el.Movie.image}
                                     />
-                                    {props.tab === 'watchlist' && (
-                                        <>
-                                            <Button content='Undo completed' labelPosition='left' icon='edit' primary id={el.id} />
-                                            <Button content='Remove' labelPosition='left' icon='edit' primary />
-                                        </>
-                                    )}
+                                    <Button content='Move back to incomplete' labelPosition='left' icon='edit' primary onClick={() => setCompletedFalse(el.id)} />
+                                    <Button content='Remove' labelPosition='left' icon='edit' primary />
 
-                                    {props.tab === 'completed' && (
-                                        <>
-                                            <Button content='Move back to incomplete' labelPosition='left' icon='edit' primary />
-                                            <Button content='Remove' labelPosition='left' icon='edit' primary />
-                                        </>
-                                    )}
                                 </Card>
                             ))
             }
         </Card.Group>
     )
 }
+
+
+{/* {props.tab === 'completed' && (
+                                        <>
+                                            <Button content='Move back to incomplete' labelPosition='left' icon='edit' primary />
+                                            <Button content='Remove' labelPosition='left' icon='edit' primary />
+                                        </>
+                                    )} */}
+
+{/* {props.tab === 'watchlist' && (
+                                        <>
+                                            <Button content='Undo completed' labelPosition='left' icon='edit' primary id={el.id} />
+                                            <Button content='Remove' labelPosition='left' icon='edit' primary />
+                                        </>
+                                    )} */}
 
 {/* <Button animated='fade' >
                                 <Button.Content visible><Icon name='trash alternate outline' /></Button.Content>
