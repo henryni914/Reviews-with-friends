f7366ed84f609c660004547c174a62e3109037d3
Henry Ni <henryni914@gmail.com>
2021-04-19 17:07:02 -0400

    testing a loader in browse section

--- client/src/components/CastTab.js
+++ client/src/components/CastTab.js
@@ -4,19 +4,20 @@ import { Grid, GridColumn, Image, List } from 'semantic-ui-react';
 export default function CastTab(props) {
     // console.log('props', props.info)
     let castResults = props.info.cast;
+    console.log(castResults)
 
     function renderGrid() {
         let renderArr = [], columns = [];
         let count = 0;
         castResults.forEach(el => {
-            if (count === 10) {
+            if (count === 10 || count === castResults.length) {
                 return renderArr;
             }
             let avatarUrl = "https://image.tmdb.org/t/p/original" + el.profile_path;
             let tmdbProfileUrl = "https://www.themoviedb.org/person/" + el.id + "?language=en-US";
             columns.push(
                 <List.Item key={el.id} href={tmdbProfileUrl} target="_blank">
-                    <Image avatar src={avatarUrl} rounded size='mini' />
+                    <Image avatar src={avatarUrl} rounded size='mini' alt={el.name}/>
                     <List.Content>
                         <List.Header>{el.name}</List.Header>
                         <List.Description>as {el.character}</List.Description>
