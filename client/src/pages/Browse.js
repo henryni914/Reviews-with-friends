1f36c5cdd10979c05fb2963e958cbb6d7321253b
Henry Ni <henryni914@gmail.com>
2021-04-19 14:16:47 -0400

    added top rated and now playing with pagination

--- client/src/pages/Browse.js
+++ client/src/pages/Browse.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Container, Grid, Pagination } from 'semantic-ui-react';
+import { Card, Container, Pagination } from 'semantic-ui-react';
 import MovieCard from '../components/MovieCard'
 import API from '../utils/API';
 
@@ -7,42 +7,53 @@ export default function Browse() {
 
     const [upcomingMovies, setUpcomingMovies] = useState([]);
     const [upcomingPage, setUpcomingPage] = useState(1);
-    const [displayUpcoming, setDisplayUpcoming] = useState([]);
+    const displayUpcoming = upcomingMovies.slice(
+        (upcomingPage - 1) * 5,
+        (upcomingPage - 1) * 5 + 5
+    )
+
     const [topRatedMovies, setTopRatedMovies] = useState([]);
     const [topRatedPage, setTopRatedPage] = useState(1);
+    const displayTopRated = topRatedMovies.slice(
+        (topRatedPage - 1) * 5,
+        (topRatedPage - 1) * 5 + 5
+    )
+
     const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
     const [nowPlayingPage, setNowPlayingPage] = useState(1)
+    const displayNowPlaying = nowPlayingMovies.slice(
+        (nowPlayingPage - 1) * 5,
+        (nowPlayingPage - 1) * 5 + 5
+    )
 
     function retrieveUpcomingMovies() {
         API.upcomingMovies().then(res => {
             setUpcomingMovies(res.data.results.slice(0, 20))
-            setDisplayUpcoming(res.data.results.slice(0, 5))
         })
     }
 
     function retrieveTopRatedMovies() {
         API.topRatedMovies().then(res => {
-            setTopRatedMovies(res.data.results.slice(0, 5))
-            // console.log(res.data)
+            setTopRatedMovies(res.data.results.slice(0, 20))
         })
     }
 
     function retrieveNowPlayingMovies() {
         API.nowPlayingMovies().then(res => {
-            setNowPlayingMovies(res.data.results.slice(0, 5))
-            // console.log(res.data)
+            setNowPlayingMovies(res.data.results.slice(0, 20))
         })
     }
 
-    function setPage(event, { activePage }) {
-        const items = 5;
-        const page = activePage;
-        const updateArr = upcomingMovies.slice(
-            (page - 1) * items,
-            (page - 1) * items + items
-        )
-        setDisplayUpcoming(updateArr)
+    function setUpcoming(event, { activePage }) {
         setUpcomingPage(activePage)
+    }
+
+    function setTopRated(event, { activePage }) {
+        setTopRatedPage(activePage)
+    }
+
+    function setNowPlaying(event, { activePage }) {
+        setNowPlayingPage(activePage)
     }
 
     useEffect(() => {
@@ -72,14 +83,14 @@ export default function Browse() {
                     firstItem={null}
                     lastItem={null}
                     totalPages={4}
-                    onPageChange={setPage}
+                    onPageChange={setUpcoming}
                 />
             </Card.Group>
 
 
-            {/* <h3>Top Rated Movies...</h3>
+            <h3>Top Rated Movies...</h3>
             <Card.Group itemsPerRow={5} stackable>
-                {topRatedMovies.map(element => (
+                {displayTopRated.map(element => (
                     <MovieCard
                         key={element.id}
                         id={element.id}
@@ -89,11 +100,20 @@ export default function Browse() {
                         release={element.release_date}
                     />
                 ))}
-            </Card.Group> */}
+                <Pagination
+                    className={'pagination'}
+                    defaultActivePage={1}
+                    activePage={topRatedPage}
+                    firstItem={null}
+                    lastItem={null}
+                    totalPages={4}
+                    onPageChange={setTopRated}
+                />
+            </Card.Group>
 
-            {/* <h3>Movies Now Playing...</h3>
+            <h3>Movies Now Playing...</h3>
             <Card.Group itemsPerRow={5} stackable>
-                {nowPlayingMovies.map(element => (
+                {displayNowPlaying.map(element => (
                     <MovieCard
                         key={element.id}
                         id={element.id}
@@ -103,7 +123,16 @@ export default function Browse() {
                         release={element.release_date}
                     />
                 ))}
-            </Card.Group> */}
+                <Pagination
+                    className={'pagination'}
+                    defaultActivePage={1}
+                    activePage={nowPlayingPage}
+                    firstItem={null}
+                    lastItem={null}
+                    totalPages={4}
+                    onPageChange={setNowPlaying}
+                />
+            </Card.Group>
         </Container>
     )
 }
