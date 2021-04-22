6d7127644a40822a827246269208e8f275e910f0
Henry Ni <henryni914@gmail.com>
2021-04-22 15:54:49 -0400

    created a like table and able to post to db

--- client/src/components/FavoriteSection.js
+++ client/src/components/FavoriteSection.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Button, Card, Container, Icon, Image, Message, Reveal } from 'semantic-ui-react';
+import { Button, Card, Container, Icon, Image, Message, Pagination, Reveal } from 'semantic-ui-react';
 import { setUserFavorites } from '../actions/user';
 import API from '../utils/API';
 
@@ -12,6 +12,17 @@ export default function CardExampleColumnCount() {
     const stateUser = useSelector(state => state.user);
     const dispatch = useDispatch();
     const [favorites, setFavorites] = useState(stateUser.favorites);
+    const [page, setPage] = useState(1)
+    const pages = Math.floor(favorites.length / 9) + 1
+    const display = favorites.slice(
+        (page - 1) * 9,
+        (page - 1) * 9 + 9
+    )
+
+
+    function handlePageChange(event, { activePage }) {
+        setPage(activePage)
+    }
 
     function deleteFavorite(id) {
         const updateArr = favorites.filter(element => element.id !== id)
@@ -26,59 +37,71 @@ export default function CardExampleColumnCount() {
             setFavorites(res.data)
             // dispatch(setUserFavorites(favorites.data))
         })
-        console.log(favorites)
+        // console.log(favorites)
     }, [])
 
+    console.log(display)
+
     return (
-        <Card.Group stackable itemsPerRow={3}>
-            {favorites.length === 0 ?
-                <Container>
-                    <Message negative>
-                        <Message.Header>Nothing to display</Message.Header>
-                        <p>Add some movies to your favorites, by clicking the heart icon on the movie page. </p>
-                    </Message>
-                </Container>
-                : favorites.map(el => (
-                    <Card >
-                        <Card.Header textAlign='center'>
-                            {/* <i>{el.Movie.title}</i> */}
-                        </Card.Header>
-                        {/* link href={`/film/id=${el.Movie.tmdbID}`} */}
-                        {/* <div className={'ui fade reveal image'}>
+        <Container >
+            <Card.Group stackable itemsPerRow={3}>
+                {favorites.length === 0 ?
+                    <Container>
+                        <Message negative>
+                            <Message.Header>Nothing to display</Message.Header>
+                            <p>Add some movies to your favorites, by clicking the heart icon on the movie page. </p>
+                        </Message>
+                    </Container>
+                    : display.map(el => (
+                        <Card >
+                            <Card.Header textAlign='center'>
+                                {/* <i>{el.Movie.title}</i> */}
+                            </Card.Header>
+                            {/* link href={`/film/id=${el.Movie.tmdbID}`} */}
+                            {/* <div className={'ui fade reveal image'}>
                             <Image src={el.Movie.image} className={'visible content'} />
                             <Image src={el.Movie.image} className={'hidden content hiddenImg'}/>
                         </div> */}
-                        <Reveal animated='small fade' >
-                            <Reveal.Content visible>
-                                <Link to={`/film/id=${el.Movie.tmdbID}`}>
+                            <Reveal animated='small fade' >
+                                <Reveal.Content visible>
+                                    <Link to={`/film/id=${el.Movie.tmdbID}`}>
+                                        <Image
+                                            size='medium'
+                                            src={el.Movie.image}
+                                            alt={el.Movie.title}
+                                        />
+                                    </Link>
+                                </Reveal.Content>
+                                <Reveal.Content hidden>
+                                    <div className='hiddenTitle'>
+                                        {el.Movie.title}
+                                    </div>
                                     <Image
+                                        className='hiddenImg'
                                         size='medium'
                                         src={el.Movie.image}
                                         alt={el.Movie.title}
                                     />
-                                </Link>
-                            </Reveal.Content>
-                            <Reveal.Content hidden>
-                                <div className='hiddenTitle'>
-                                    {el.Movie.title}
-                                </div>
-                                <Image
-                                    className='hiddenImg'
-                                    size='medium'
-                                    src={el.Movie.image}
-                                    alt={el.Movie.title}
-                                />
-                            </Reveal.Content>
-                        </Reveal>
+                                </Reveal.Content>
+                            </Reveal>
 
-                        <Button animated='fade' floated='right' onClick={() => deleteFavorite(el.id)}>
-                            <Button.Content visible><Icon name='trash alternate outline' /></Button.Content>
-                            <Button.Content hidden>Remove</Button.Content>
-                        </Button>
-                    </Card>
-                ))
-            }
-        </Card.Group>
+                            <Button animated='fade' floated='right' onClick={() => deleteFavorite(el.id)}>
+                                <Button.Content visible><Icon name='trash alternate outline' /></Button.Content>
+                                <Button.Content hidden>Remove</Button.Content>
+                            </Button>
+                        </Card>
+                    ))
+                }
+                < Pagination
+                    className={'paginationProfile'}
+                    activePage={page}
+                    firstItem={null}
+                    lastItem={null}
+                    totalPages={pages}
+                    onPageChange={handlePageChange}
+                />
+            </Card.Group>
+        </Container>
     )
 }
 
