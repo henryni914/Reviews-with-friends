5fa7fce3ae17d384d0e84450975dd09b89ed5bc7
Henry Ni <henryni914@gmail.com>
2021-04-23 20:20:03 -0400

    added rds in config.js

--- config/config.js
+++ config/config.js
@@ -1,21 +1,29 @@
 require('dotenv').config();
 module.exports = {
-"development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-},
-"test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-},
-"production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  }
+    "development": {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "production": {
+        "username": process.env.RDS_USERNAME,
+        "password": process.env.RDS_PASSWORD,
+        "port": process.env.RDS_PORT,
+        "database": process.env.RDS_DATABASE,
+        "host": process.env.RDS_HOSTNAME,
+        "dialect": "mysql",
+        "dialectOptions": {
+            ssl: 'Amazon RDS'
+        },
+        "language": 'en'
+    }
 };
